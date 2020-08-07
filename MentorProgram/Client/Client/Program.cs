241d23ec40c2647d29782ab7912c5c055974a959
RXHD <prograshid@gmail.com>
2020-08-07 19:54:20 +0600

    Added chat

---
+++ MentorProgram/Client/Client/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        
+        static string[] names = new string[] { "Liam", "Noah", "Mason", "Ethan", "Logan", "Lucas", "Jackson", "Aiden", "Oliver", "Jacob" };
+        static string[] messages = new string[] { "Привет!", "/exit", "Как дела?", "Как погода?", "Что делаешь?", "Как настроение?", 
+            "Здравствуйте", "Добрый день!", "Всем пока", "До встречи" };
+        static Random rnd = new Random();
+        static CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        static CancellationToken token = cancelTokenSource.Token;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect("127.0.0.1", 2048);
+              
+                Console.WriteLine("Успешное подключение к серверу");
+
+                clientSocket.Send(Encoding.Unicode.GetBytes(names[rnd.Next(0, 10)]));
+
+                byte[] historyMessagesBuff = new byte[1024];
+                string[] historyMessages = Encoding.Unicode.GetString(historyMessagesBuff, 0, clientSocket.Receive(historyMessagesBuff))
+                                                                                .Split(new char[] { '~' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in historyMessages)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (true)
+                    {
+                        if (!token.IsCancellationRequested)
+                        {
+                            byte[] realTimeMessagesBuff = new byte[1024];
+                            Console.WriteLine(Encoding.Unicode.GetString(realTimeMessagesBuff, 0, clientSocket.Receive(realTimeMessagesBuff)));
+
+                        }
+                        else
+                        {
+                            clientSocket.Shutdown(SocketShutdown.Both);
+                            clientSocket.Close();
+                            break;
+                        }
+                    }
+                }, token);
+
+                while (true)
+                {
+                    Thread.Sleep(rnd.Next(0, 1000));
+                    string message = messages[rnd.Next(0, 9)]; 
+                    
+                    clientSocket.Send(Encoding.Unicode.GetBytes(message));
+                    if (message == "/exit")
+                    {
+                        cancelTokenSource.Cancel();
+                        break;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ MentorProgram/Server/Server/Program.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        private static List<Socket> clients = new List<Socket>();
+        private static List<string> messages = new List<string>();
+        private static List<User> users = new List<User>();
+        private static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        class User
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сервер запущен...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 2048));
+            serverSocket.Listen(10);
+            serverSocket.BeginAccept(AcceptCallBack, null);
+           
+            Console.ReadLine();
+        }
+
+        private static void AcceptCallBack(IAsyncResult asyncResult)
+        {
+            Socket socket = serverSocket.EndAccept(asyncResult);
+            clients.Add(socket);
+            Task task = new Task(HandleClient, socket);
+            task.Start();
+            serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+
+        static void HandleClient(object o)
+        {
+            Socket client = (Socket)o;
+            StringBuilder builder = new StringBuilder();
+            byte[] name = new byte[1024];
+            try
+            {
+                builder.Append(Encoding.Unicode.GetString(name, 0, client.Receive(name)));
+                users.Add(new User()
+                {
+                    Id = client.RemoteEndPoint.ToString(),
+                    Name = builder.ToString()
+                });
+                Console.WriteLine($"К серверу подключился {builder}");
+                if (messages.Count > 0)
+                {
+                    client.Send(Encoding.Unicode.GetBytes(String.Join("~", messages.Skip(messages.Count - 5))));
+                }
+                else
+                {
+                    client.Send(Encoding.Unicode.GetBytes("Прошлых сообщений нет"));
+                }
+                while (true)
+                {
+                    builder.Clear();
+                    byte[] messageBuff = new byte[1024];
+                    var userName = users.Where(x => x.Id == client.RemoteEndPoint.ToString()).FirstOrDefault().Name;
+                    try
+                    {
+                        builder.Append(Encoding.Unicode.GetString(messageBuff, 0, client.Receive(messageBuff)));
+                        if (builder.ToString() != "/exit")
+                        {
+
+                            messages.Add($"{userName}: {builder}");
+                            Console.WriteLine($"{userName}: {builder}");
+                            foreach (var c in clients)
+                            {
+                                if (c != client)
+                                {
+                                    c.Send(Encoding.Unicode.GetBytes($"{userName}: {builder}"));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{userName} покинул чат");
+                            clients.Remove(client);
+                            client.Shutdown(SocketShutdown.Both);
+                            client.Disconnect(true);
+                            break;
+                        }
+                    }
+                    catch (SocketException)
+                    {
+                        clients.Remove(client);
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Disconnect(true);
+
+                        Console.WriteLine($"{userName} покинул чат");
+                        break;
+                    }
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Что-то пошло не так");
+                
+            }
+            
+        }
+    }
+}
